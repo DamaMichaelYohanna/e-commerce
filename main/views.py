101d101fa95b6ddaf6d4502744e42992f804e385
DamaMichaelYohanna <get2dama11@gmail.com>
2022-09-15 14:57:04 +0100

    updated views and template

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Category
 
 
 def index(request):
@@ -9,8 +9,26 @@ def index(request):
         print(search_key)
         search_1 = Product.objects.filter(name__icontains=search_key)
         print(search_1)
-    men_and_women_fashion = Product.objects.all()
-    context = {'fashion': men_and_women_fashion}
+    all_products = Product.objects.all()  # grab all product
+    all_category = Category.objects.all()  # grab all the categories
+    # filter sub categories from all category.
+    fashion_category = all_category.get(name='Fashion')  # grab fashion sub cat
+    electronic_category = all_category.get(name='Electronics')  # grab fashion sub cat
+    sport_category = all_category.get(name='Sport')  # grab fashion sub cat
+    # filter products now base on categories
+    # for item in fashion_category:
+    #     sub_cat = fashion_category.
+    # fashion = [fashion_category]
+    # # parent_cat = fashion[0].parent
+    # while fashion_category is not None:
+    #     fashion.append(fashion_category.name)
+    #     print(fashion_category.name)
+    #     # k = k.parent
+    print(fashion_category.children.all())
+    fashion = all_products.filter(category__in=fashion_category.children.all())
+    electronics = all_products.filter(category__in=electronic_category.children.all())
+    sport_and_out_door = all_products.filter(category__in=sport_category.children.all())
+    context = {'fashion': fashion, 'sport': sport_and_out_door, 'electronic': electronics}
     return render(request, 'main/home.html', context)
 
 
