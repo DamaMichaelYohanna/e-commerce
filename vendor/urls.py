140a1909d2230174b4aa48788d65442f4f8fe83d
DamaMichaelYohanna <get2dama11@gmail.com>
2022-09-22 17:07:56 +0100

    updated url and template

--- vendor/urls.py
+++ vendor/urls.py
@@ -6,7 +6,7 @@ app_name = 'vendor'
 urlpatterns = [
     path('', views.dashboard, name='vendor dashboard'),
     path('product/add', views.add_product, name='v_add_product'),
-    path('product/update', views.update_product, name='v_update_product'),
-    path('product/delete', views.delete_product, name='v_delete_product'),
+    path('product/<int:pk>/update', views.update_product, name='v_update_product'),
+    path('product/<int:pk>/delete', views.delete_product, name='v_delete_product'),
 
 ]
